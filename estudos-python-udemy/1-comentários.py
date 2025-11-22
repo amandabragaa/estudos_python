1c11158d27ae8cd7afbc8d5c5877f02613f050d7
Amanda Braga <amandabraga1998@gmail.com>
2025-11-22 12:08:03 -0300

    Estudos sobre funções lambda e built in function

---
+++ estudos-python-udemy/1-comentários.py
@@ -0,0 +1,9 @@
+
+#Este código imprime o resultado -> só comenta o que está a frente do #
+# Escrevo frases mais curtas e rápidas
+
+""" Este código imprime o resultado.
+Se eu der entender segue comentando normal.
+Consigo escrever um texto mais longo """ 
+
+print("Olá, meu nome é Amanda") 
