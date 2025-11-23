3a20dd44ddc6f727ecd4b03bcb8827ba86c36fd0
Amanda Braga <amandabraga1998@gmail.com>
2025-11-23 09:43:05 -0300

    Estudo sobre funções, operadores e modulos

---
+++ estudos-python-udemy/4.1-retorno-funcoes.py
@@ -0,0 +1,11 @@
+# exemplo de declaração de função
+def soma(a,b):
+    return a + b
+
+# devemos sempre cha,mar nossas funções e colocar ps parametros que criamos, 
+# assim toda vez que queremos fazer uma soma não precisa programas, basta 
+# chamar o nome da função e passar os valores que queira somar igual ao exemplo: 
+result = soma(10,30)
+print(result)
+# ou
+print(soma(10, 30))
